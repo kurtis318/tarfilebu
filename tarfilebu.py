8cc10ea5481157e29e4dc3c0e64ea3546c177e7e
kurtis <kurtis318@gmail.com>
2017-08-28 12:30:56 -0500

    main script

---
+++ tarfilebu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+def main():
+    #    test1()
+    #    test2()
+    print("Running main() function")
+
+    return 0
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    exit(0)
