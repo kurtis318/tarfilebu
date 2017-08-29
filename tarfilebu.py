664fa7514488254e66f0b41b4f02de8380ace0d3
kurtis <kurtis318@gmail.com>
2017-08-28 22:36:24 -0500

    Started parsing command line parameters

--- tarfilebu.py
+++ tarfilebu.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 
+import sys
+from RunCmdPy import RunCmd
+
+def parse_parms(cmd_parms):
+    """
+
+    :param cmd_parms: Command-line parameters
+    :type cmd_parms:  array of strings
+    :return: nothing
+    :rtype: none
+    """
+    cmd_parm_cnt = len(cmd_parms)-1
+    print(">>>parse_parms(.): Starting")
+    print("   This is the name/path of the script:{0}".format(cmd_parms[0]))
+    print("   Number of arguments:{0}".format(cmd_parm_cnt))
+    print("   Argument List: {}".format(str(cmd_parms)))
+    print(">>>parse_parms(.): Finished")
 
 def main():
     #    test1()
     #    test2()
-    print("Running main() function")
+    print("\nRunning main() function")
+    parse_parms(sys.argv)
 
     return 0
 
